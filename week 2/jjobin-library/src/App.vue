83a2d2d631a8f5fef440831a4e81ad959325b14e
Janice Jobin <janicejobin@Janices-MacBook-Air.local>
2024-08-02 23:45:12 +1000

    first commit

---
+++ week 2/jjobin-library/src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+import JSON from './components/JSON.vue'
+</script>
+
+<template>
+  <header>
+  </header>
+
+  <main>
+    <JSON></JSON>
+  </main>
+</template>
+
+<style scoped>
+
+
+</style>
