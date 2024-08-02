14f3362a6c769b181ebbb3a26129b9d21d3714ee
Janice Jobin <janicejobin@Janices-MacBook-Air.local>
2024-08-02 23:52:06 +1000

    week 1 updated

---
+++ week1/src/components/MyName.vue
@@ -0,0 +1,22 @@
+<script setup>
+defineProps({
+  name: {
+    type: String
+  }
+})
+</script>
+
+<template>
+    <div class="displayName">
+        <h1 class="green">
+            {{ name }}
+        </h1>
+    </div>
+</template>
+
+<style>
+.displayName h1 {
+  text-align: center;
+}
+
+</style>
