9dca37653cc15cfe82af6e7df3ef50b6cd4f3774
Janice Jobin <janicejobin@Janices-MacBook-Air.local>
2024-08-16 02:26:57 +1000

    e-portfolio week 3&4 tasks

---
+++ week 3/jjobin-library/src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+import JSON from './components/JSON.vue'
+import LoginForm from './components/LoginForm.vue';
+
+</script>
+
+<template>
+  <header>
+  </header>
+
+  <main>
+    <LoginForm></LoginForm>
+  </main>
+</template>
+
+<style scoped>
+
+
+</style>
